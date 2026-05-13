f79e2d8c1d8b5c4a596f0fd58ee22b5b6f6ea8a7
toma.jalagania <toma.jalagania@gmail.com>
2026-05-13 16:20:09 +0400

    maped user model

--- app/models/User.py
+++ app/models/User.py
@@ -1,5 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase,Mapped,mapped_column
 
-class User(DeclarativeBase):
-    pass
+class Base(DeclarativeBase):
+    id:Mapped[int] = mapped_column(primary_key=True)
+
+
+class User(Base):
+    Username:Mapped[str]
+    Email:Mapped[str] = mapped_column(unique=True)
